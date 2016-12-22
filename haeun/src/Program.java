caa8eb28ea6ed5d4851faff89c2f57f12d5e837a
phe14 <peri9508@naver.com>
2016-12-22 16:46:56 +0900

    변수이름 수정

--- haeun/src/Program.java
+++ haeun/src/Program.java
@@ -6,16 +6,17 @@ import javax.swing.*;
 import javax.swing.event.*;
 
 public class Program extends JFrame implements ActionListener, ListSelectionListener {
-	JFrame phonebookAddFrame;
-	JPanel accountPanel, phonebookPanel, schedulePanel, phonebookButtonPanel, phonebookViewPanel;
-	JButton phonebookAddButton, phonebookAllRemoveButton, phonebookRemoveButton;
+	JFrame phonebookAddFrame, ScheduleAddFrame;
+	JPanel accountPanel, phonebookPanel, phonebookButtonPanel, phonebookViewPanel,
+			schedulePanel, scheduleButtonPanel, scheduleViewPanel;
+	JButton phonebookAddButton, phonebookAllRemoveButton, phonebookRemoveButton,
+			scheduleAddButton, scheduleAllRemoveButton, scheduleRemoveButton;
 
 	public Program() throws IOException {
 		setupAccountFrame();
 		setupPhonebookFrame();
 		setupScheduleFrame();
 		setupFrame();
-		
 	}
 
 	public void setupAccountFrame() {
@@ -28,7 +29,6 @@ public class Program extends JFrame implements ActionListener, ListSelectionList
 		phonebookPanel.setPreferredSize(new Dimension(540, 500));
 		phonebookPanel.setBorder(BorderFactory.createTitledBorder("phonebook"));
 
-		// 추가, 전체삭제 버튼 패널
 		phonebookButtonPanel = new JPanel();
 		phonebookButtonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 0));
 		phonebookButtonPanel.setPreferredSize(new Dimension(530, 50));
@@ -44,13 +44,28 @@ public class Program extends JFrame implements ActionListener, ListSelectionList
 		phonebookAddButton.addActionListener(this);
 		phonebookRemoveButton.addActionListener(this);
 		phonebookAllRemoveButton.addActionListener(this);
-
 	}
 
 	public void setupScheduleFrame() {
 		schedulePanel = new JPanel();
 		schedulePanel.setPreferredSize(new Dimension(540, 500));
 		schedulePanel.setBorder(BorderFactory.createTitledBorder("schedule"));
+
+		scheduleButtonPanel = new JPanel();
+		scheduleButtonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 0));
+		scheduleButtonPanel.setPreferredSize(new Dimension(530, 50));
+
+		scheduleAddButton = new JButton("추가");
+		scheduleRemoveButton = new JButton("삭제");
+		scheduleAllRemoveButton = new JButton("전체삭제");
+		scheduleButtonPanel.add(scheduleAddButton);
+		scheduleButtonPanel.add(scheduleRemoveButton);
+		scheduleButtonPanel.add(scheduleAllRemoveButton);
+		schedulePanel.add(scheduleButtonPanel, BorderLayout.NORTH);
+
+		scheduleAddButton.addActionListener(this);
+		scheduleRemoveButton.addActionListener(this);
+		scheduleAllRemoveButton.addActionListener(this);
 	}
 
 	public void setupFrame() {
@@ -72,13 +87,10 @@ public class Program extends JFrame implements ActionListener, ListSelectionList
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
-
 }
